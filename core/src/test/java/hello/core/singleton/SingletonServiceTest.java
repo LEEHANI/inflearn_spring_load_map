d1e4a3ad2df61b2a96f2d85683151e1c48b252e6
leehani <rorean@naver.com>
2021-08-11 14:24:01 +0900

    싱글톤 컨테이너

---
+++ core/src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonServiceTest {
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same ==
+        //equal
+    }
+}
